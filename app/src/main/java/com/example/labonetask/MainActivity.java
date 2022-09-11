8149c95a63a8de9d482e4cb66692269ab101bb5e
TuranCSKT <turan.qarashli17@gmail.com>
2022-09-11 02:23:12 -0700

    The functionality to change Text view was added

--- app/src/main/java/com/example/labonetask/MainActivity.java
+++ app/src/main/java/com/example/labonetask/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.labonetask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnClick(View view) {
+        this.tvMain.setText("Information: Text was changed!!!");
     }
 }
